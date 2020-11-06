b54e0649957e5711727457f590a600acf744e3e4
benjaminshu2001 <benjaminshu2001@gmail.com>
2020-11-05 16:18:28 -0800

    Fixed main.yml and headers for test suites

--- Area_passing_tests.h
+++ Area_passing_tests.h
@@ -1,7 +1,7 @@
 #ifndef __AREA_PASSING_TESTS_HPP__
 #define __AREA_PASSING_TESTS_HPP__
 
-#include "rectangle.cpp"
+
 
 TEST(AreaTest, AreaNonZero) {
 	Rectangle r1 = Rectangle(2.0, 4.0);
@@ -16,11 +16,6 @@ TEST(AreaTest, AreaZero) {
 TEST(AreaTest, AreaNegative) {
 	Rectangle r1 = Rectangle(-4.0, 5.0);
 	EXPECT_EQ(r1.area(), 20.0);
-}
-
-TEST(AreaTest, AreaNoInput) {
-	Rectangle r1 = Rectangle("","");
-	EXPECT_EQ(r1.area(), "");
 }
 
 #endif
