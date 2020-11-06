c9b67d27f651898b21b8c753365c74fb78d8137c
Kirby-Vong <kirbyvong@gmail.com>
2020-11-05 16:56:58 -0800

    Add passing tests

---
+++ Area_passing_tests.h
@@ -0,0 +1,27 @@
+#ifndef __AREA_PASSING_TESTS_HPP__
+#define __AREA_PASSING_TESTS_HPP__
+
+#include "rectangle.cpp"
+
+TEST(AreaTest, AreaNonZero) {
+    Rectangle r1 = Rectangle(2.0, 4.0);
+    EXPECT_EQ(r1.area(), 8.0);
+}
+
+TEST(AreaTest, AreaZero) {
+    Rectangle r1 = Rectangle(5.0,0.0);
+    EXPECT_EQ(r1.area(), 0.0);
+}
+
+TEST(AreaTest, AreaNegative) {
+    Rectangle r1 = Rectangle(-4.0, 5.0);
+    EXPECT_EQ(r1.area(), 20.0);
+}
+
+TEST(AreaTest, AreaNoInput) {
+    Rectangle r1 = Rectangle("","");
+    EXPECT_EQ(r1.area(), "");
+}
+
+#endif
+								 
---
+++ Perimeter_passing_tests.h
@@ -0,0 +1,22 @@
+#ifndef __PERIMETER_PASSING_TESTS_HPP__
+#define __PERIMETER_PASSING_TESTS_HPP__
+
+
+
+TEST(PerimeterTest, PerimeterNonZero) {
+	Rectangle r1 = Rectangle(3, 5);
+	EXPECT_EQ(r1.perimeter(), 16);
+}
+
+TEST(PerimeterTest, PerimeterNegative) {
+	Rectangle r1 = Rectangle(-5,5);
+	EXPECT_EQ(r1.perimeter(), 20);
+}
+
+TEST(PerimeterTest, PerimeterZero) {
+	Rectangle r1 = Rectangle(0,0);
+	EXPECT_EQ(r1.perimeter(), 0);
+}						}
+
+#endif
+
